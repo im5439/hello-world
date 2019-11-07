59b57ea332e7c8b4845e13bceda135999492a13a
임충환 <@DESKTOP-QSSQ0G0>
2019-11-07 22:02:42 +0900

    hi world 메시지 추가

--- src/pjtTest/MainClass.java
+++ src/pjtTest/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Java World!!!!!!!!!!!!");
-		
+		System.out.println("Hello Java World");
+		System.out.println("Hi world");
 	}
 }
